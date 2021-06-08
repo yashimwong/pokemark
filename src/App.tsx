3b61fc2ebae829b8cfd3fbdd2cb9c90c83ead99e
yashimwong <yashimwong@gmail.com>
2021-06-08 23:32:45 +0800

    feat: add initial router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "components/Navigation";
 import Home from "pages/Home";
+import Settings from "pages/Settings";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navigation version="0.01" />
-      <Home />
-    </>
+      <Switch>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,27 @@
+import { Link } from "react-router-dom";
+
 type NavigationProps = {
   version: String;
 };
 
 const Navigation = ({ version }: NavigationProps) => {
   return (
-    <nav className="flex flex-row flex-nowrap items-center justify-start h-12 bg-gray-900">
+    <div className="flex flex-row flex-nowrap items-center justify-start h-12 bg-gray-900">
       <div className="flex flex-row items-center ml-4 text-gray-50 text-2xl leading-none">
         PokeMark
         <div className="self-end text-gray-400 ml-1 text-base leading-none">
           v{version}
         </div>
       </div>
-    </nav>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/settings">Settings</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
