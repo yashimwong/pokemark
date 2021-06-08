424e70e6561fa40d65ca3f78b82d3c7bacb2d01a
yashimwong <yashimwong@gmail.com>
2021-06-09 00:21:12 +0800

    feat: add links to branding and stylized navigation btn

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { CogIcon } from "@heroicons/react/outline";
 
 type NavigationProps = {
   version: String;
@@ -8,16 +9,19 @@ const Navigation = ({ version }: NavigationProps) => {
   return (
     <div className="flex flex-row flex-nowrap items-center justify-start h-12 bg-gray-900">
       <div className="flex flex-row items-center ml-4 text-gray-50 text-2xl leading-none">
-        PokeMark
+        <Link to="/">PokeMark</Link>
         <div className="self-end text-gray-400 ml-1 text-base leading-none">
           v{version}
         </div>
       </div>
 
-      <nav>
+      <nav className="flex flex-row flex-nowrap items-center justify-end w-full mr-4">
         <ul>
-          <li>
-            <Link to="/settings">Settings</Link>
+          <li className="text-gray-50 bg-gray-700 py-1 px-2 rounded-md hover:bg-gray-600">
+            <Link to="/settings">
+              <CogIcon className="inline h-5 w-5 mr-1 mb-0.5" />
+              Settings
+            </Link>
           </li>
         </ul>
       </nav>
