9eea1aa2fdebc466beb3177eea75f4fb877d3062
yashimwong <yashimwong@gmail.com>
2021-06-14 19:54:12 +0800

    feat: add pokemon unit selection (menu)

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,30 @@
+enum Height {
+  metre = "metre",
+  feet = "feet",
+}
+
 const Settings = () => {
-  return <h1>Settings Page</h1>;
+  return (
+    <div className="flex flex-row justify-center items-start h-screen">
+      <div className="w-8/12 shadow mt-8">
+        <div className="bg-gray-900 p-4">
+          <h1 className="text-gray-50 text-lg">Settings</h1>
+        </div>
+        <div className="py-2 px-4">
+          Height Units:
+          <select>
+            {Object.keys(Height).map((k) => {
+              return (
+                <option key={k} value={k}>
+                  {k}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
