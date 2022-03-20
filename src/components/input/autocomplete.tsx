bfe18eeb6c631e3053a372d49774a8b15655bd81
yashimwong <yashimwong@gmail.com>
2022-03-20 23:41:54 +0800

    feat: add filter by typing

--- src/components/input/autocomplete.tsx
+++ src/components/input/autocomplete.tsx
@@ -1,17 +1,32 @@
 import React, { HTMLAttributes } from "react";
 
+type Data = {
+    key: string;
+    value: string;
+};
+
 interface AutoComplete extends HTMLAttributes<HTMLInputElement> {
-    data: Array<{ key: string; value: string }>;
+    data: Data[];
 }
 
 const AutoComplete = ({ data, ...props }: AutoComplete) => {
     const [input_value, setInputValue] = React.useState("");
+    const [filtered, setFiltered] = React.useState<Data[]>([]);
+
+    const handleKeyPress = () => {
+        setFiltered(data.filter((d) => new RegExp(input_value).test(d.key)));
+    };
 
     return (
         <div>
-            <input className="" value={input_value} onChange={(e) => setInputValue(e.target.value)} />
+            <input
+                className=""
+                value={input_value}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleKeyPress}
+            />
             <div>
-                {data.map(({ key, value }) => {
+                {filtered.map(({ key, value }) => {
                     return <div key={key}>{key}</div>;
                 })}
             </div>
