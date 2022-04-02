b9959cf223e6a77e4ac46185a81a51cdd21d3bad
yashimwong <yashimwong@gmail.com>
2022-04-02 16:22:38 +0800

    chore: cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "pages/home";
-import GlobalStates from "stores/global-data";
 import "styles/main.css";
 
 const App = () => {
     return (
-        <GlobalStates>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
-        </GlobalStates>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
