37caae50b379d28988bee3d6a3be0e2619107368
yashimwong <yashimwong@gmail.com>
2022-04-03 21:46:58 +0800

    feat: add trainer wizard

---
+++ src/components/form/trainer-wizard.tsx
@@ -0,0 +1,9 @@
+const TrainerWizard = () => {
+    return (
+        <div className="w-full mt-8">
+            <div className="w-11/12 shadow-lg mx-auto">Wizard</div>
+        </div>
+    );
+};
+
+export default TrainerWizard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,13 @@
+import TrainerWizard from "components/form/trainer-wizard";
 import MainContainer from "components/layout/container";
 
 const Home = () => {
     return (
         <MainContainer>
-            <h1 className="text-2xl">Homepage</h1>
+            <div className="flex-col w-full">
+                <h1 className="text-2xl">Homepage</h1>
+                <TrainerWizard />
+            </div>
         </MainContainer>
     );
 };
