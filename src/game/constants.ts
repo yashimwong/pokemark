8bb6fe9a2cd0c0cbba995f5b8fb455f3203b6b3f
yashimwong <yashimwong@gmail.com>
2022-04-07 10:15:00 +0800

    feat: add game domain types

---
+++ src/game/constants.ts
@@ -0,0 +1,5 @@
+export const mapWidth = 16;
+export const mapHeight = 12;
+export const maxPartySize = 6;
+export const grassEncounterChance = 0.18;
+export const experienceForLevel = (level: number) => level * 24;
---
+++ src/game/types.ts
@@ -0,0 +1,89 @@
+export type PokemonType = "normal" | "fire" | "water" | "grass" | "electric" | "bug" | "poison" | "ground" | "flying" | "psychic" | "rock";
+
+export type MoveCategory = "physical" | "special" | "status";
+
+export type Move = {
+    id: string;
+    name: string;
+    type: PokemonType;
+    power: number;
+    accuracy: number;
+    pp: number;
+    category: MoveCategory;
+    description: string;
+};
+
+export type PokemonSpecies = {
+    id: string;
+    name: string;
+    number: string;
+    types: PokemonType[];
+    color: string;
+    sprite: string;
+    baseHp: number;
+    attack: number;
+    defense: number;
+    speed: number;
+    moves: string[];
+    evolvesTo?: string;
+    evolutionLevel?: number;
+};
+
+export type Pokemon = {
+    uid: string;
+    speciesId: string;
+    nickname: string;
+    level: number;
+    experience: number;
+    hp: number;
+    maxHp: number;
+    moves: Move[];
+};
+
+export type PlayerPosition = {
+    x: number;
+    y: number;
+};
+
+export type Trainer = {
+    id: string;
+    name: string;
+    title: string;
+    greeting: string;
+    defeatText: string;
+    x: number;
+    y: number;
+    direction: "up" | "down" | "left" | "right";
+    pokemon: Pokemon[];
+    reward: number;
+};
+
+export type BattleParticipant = {
+    kind: "wild" | "trainer";
+    trainer?: Trainer;
+    pokemon: Pokemon[];
+};
+
+export type BattleState = {
+    opponent: BattleParticipant;
+    activePlayerIndex: number;
+    activeOpponentIndex: number;
+    message: string;
+    turn: "player" | "opponent" | "finished";
+    canRun: boolean;
+};
+
+export type GameScreen = "overworld" | "battle" | "starter" | "intro";
+
+export type GameState = {
+    screen: GameScreen;
+    trainerName: string;
+    playerPosition: PlayerPosition;
+    party: Pokemon[];
+    storage: Pokemon[];
+    money: number;
+    badges: string[];
+    defeatedTrainers: string[];
+    battle?: BattleState;
+    notice?: string;
+};
