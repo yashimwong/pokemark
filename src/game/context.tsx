dc116caa736382955dc56bfc31e6d63842d62054
yashimwong <yashimwong@gmail.com>
2022-05-08 10:15:00 +0800

    feat: add game state provider

---
+++ src/game/context.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import { chooseStarter, initialGameState, movePlayer, runFromBattle, setTrainerName, switchPokemon, useMove, catchPokemon, dismissNotice } from "game/state";
+import { GameState, Move } from "game/types";
+
+type GameContextValue = {
+    game: GameState;
+    beginAdventure: (name: string) => void;
+    selectStarter: (speciesId: string) => void;
+    move: (dx: number, dy: number) => void;
+    fight: (move: Move) => void;
+    switchPartyMember: (index: number) => void;
+    run: () => void;
+    catchWildPokemon: () => void;
+    clearNotice: () => void;
+};
+
+const GameContext = createContext<GameContextValue | undefined>(undefined);
+
+export const GameProvider = ({ children }: { children: ReactNode }) => {
+    const [game, setGame] = useState(initialGameState);
+    const value = useMemo(() => ({
+        game,
+        beginAdventure: (name: string) => setGame((state) => setTrainerName(state, name)),
+        selectStarter: (speciesId: string) => setGame((state) => chooseStarter(state, speciesId)),
+        move: (dx: number, dy: number) => setGame((state) => movePlayer(state, dx, dy)),
+        fight: (move: Move) => setGame((state) => useMove(state, move)),
+        switchPartyMember: (index: number) => setGame((state) => switchPokemon(state, index)),
+        run: () => setGame((state) => runFromBattle(state)),
+        catchWildPokemon: () => setGame((state) => catchPokemon(state)),
+        clearNotice: () => setGame((state) => dismissNotice(state))
+    }), [game]);
+
+    return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
+};
+
+export const useGame = () => {
+    const context = useContext(GameContext);
+    if (!context) throw new Error("useGame must be used inside GameProvider");
+    return context;
+};
