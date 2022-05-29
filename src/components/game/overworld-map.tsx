75b5036c0655995b21411abf9915c16d0d6efe8d
yashimwong <yashimwong@gmail.com>
2022-05-29 10:15:00 +0800

    feat: render meadow town overworld

---
+++ src/components/game/overworld-map.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import classNames from "classnames";
+import { townMap, trainers } from "game/data/world";
+import { GameState } from "game/types";
+
+const tileSymbols = { tree: "♣", water: "≈", flower: "✿", sign: "!", lab: "⌂", healing: "✚", bridge: "", path: "", grass: "" };
+
+const OverworldMap = ({ game, onMove }: { game: GameState; onMove: (dx: number, dy: number) => void }) => {
+    useEffect(() => {
+        const moveForKey: Record<string, [number, number]> = { ArrowUp: [0, -1], w: [0, -1], W: [0, -1], ArrowDown: [0, 1], s: [0, 1], S: [0, 1], ArrowLeft: [-1, 0], a: [-1, 0], A: [-1, 0], ArrowRight: [1, 0], d: [1, 0], D: [1, 0] };
+        const handleKeyDown = (event: KeyboardEvent) => {
+            const direction = moveForKey[event.key];
+            if (direction) {
+                event.preventDefault();
+                onMove(direction[0], direction[1]);
+            }
+        };
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [onMove]);
+
+    return (
+        <div className="map-frame">
+            <div className="world-map" style={{ gridTemplateColumns: `repeat(${townMap[0].length}, minmax(0, 1fr))` }}>
+                {townMap.flatMap((row, y) => row.map((tile, x) => {
+                    const trainer = trainers.find((item) => item.x === x && item.y === y && !game.defeatedTrainers.includes(item.id));
+                    const player = game.playerPosition.x === x && game.playerPosition.y === y;
+                    return <div key={`${x}-${y}`} className={classNames("map-tile", `tile-${tile}`)}>{tileSymbols[tile]}{trainer && <span className="map-trainer">●</span>}{player && <span className="map-player">●</span>}</div>;
+                }))}
+            </div>
+        </div>
+    );
+};
+
+export default OverworldMap;
