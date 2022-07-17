a9a3ca2f5dea1bf758e188ce8fcc8a21df5af9e5
yashimwong <yashimwong@gmail.com>
2022-07-17 10:15:00 +0800

    feat: refresh adventure navigation

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,20 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavigationBar = () => {
+    const linkClass = ({ isActive }: { isActive: boolean }) => `px-3 py-1.5 rounded-lg text-sm font-bold transition ${isActive ? "bg-white text-slate-900" : "text-slate-300 hover:text-white"}`;
     return (
-        <nav className="fixed top-0 w-full h-12 inline-flex justify-center items-center bg-black text-white">
-            <div className="w-11/12 inline-flex">
-                <div className="text-2xl">PokeMark</div>
-                <ul className="inline-flex items-center ml-auto">
-                    <li className="mr-2">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="mr-2">
-                        <Link to="/roster">Roster</Link>
-                    </li>
-                    <li className="mr-2">
-                        <Link to="/settings">Settings</Link>
-                    </li>
+        <nav className="fixed top-0 z-20 w-full h-14 inline-flex justify-center items-center bg-slate-950 text-white shadow-lg">
+            <div className="w-11/12 max-w-6xl inline-flex items-center">
+                <div className="text-xl font-black tracking-tight">⚡ PokeMark</div>
+                <ul className="inline-flex items-center gap-1 ml-auto">
+                    <li><NavLink to="/" className={linkClass}>Adventure</NavLink></li>
+                    <li><NavLink to="/roster" className={linkClass}>Roster</NavLink></li>
+                    <li><NavLink to="/settings" className={linkClass}>Journal</NavLink></li>
                 </ul>
             </div>
         </nav>
