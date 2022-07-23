12d2acdb43b62e1e91e01f5e6163e69a088665ed
yashimwong <yashimwong@gmail.com>
2022-07-23 10:15:00 +0800

    style: widen game layout

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -3,8 +3,8 @@ import classNames from "classnames";
 
 const MainContainer = ({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) => {
     return (
-        <div className={classNames("flex justify-center w-full mt-12", className)} {...props}>
-            <div className="flex w-11/12">{children}</div>
+        <div className={classNames("flex justify-center w-full mt-14", className)} {...props}>
+            <div className="flex w-11/12 max-w-6xl">{children}</div>
         </div>
     );
 };
