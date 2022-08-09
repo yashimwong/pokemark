fb07fd5e8fe071e223c0ecebd46ba357b42d71c2
yashimwong <yashimwong@gmail.com>
2022-08-09 10:15:00 +0800

    feat: add world map legend

---
+++ src/components/game/world-legend.tsx
@@ -0,0 +1,11 @@
+const WorldLegend = () => (
+    <div className="flex flex-wrap gap-x-4 gap-y-2 text-xs text-slate-500 mt-4">
+        <span><b className="text-green-700">Green</b> tall grass</span>
+        <span><b className="text-blue-600">Blue</b> water</span>
+        <span><b className="text-red-500">●</b> trainer</span>
+        <span><b className="text-indigo-600">●</b> you</span>
+        <span><b className="text-rose-500">✚</b> healing station</span>
+    </div>
+);
+
+export default WorldLegend;
