d3ed8292dd8ce902d4d1c7d05ae9b7c5fb0697f0
yashimwong <yashimwong@gmail.com>
2022-10-08 10:15:00 +0800

    style: add restrained vitality meters

--- src/components/game/health-bar.tsx
+++ src/components/game/health-bar.tsx
@@ -1,12 +1,10 @@
-import classNames from "classnames";
-
 const HealthBar = ({ current, max, compact }: { current: number; max: number; compact?: boolean }) => {
     const percent = Math.max(0, Math.round(current / max * 100));
-    const color = percent > 50 ? "bg-green-500" : percent > 20 ? "bg-yellow-400" : "bg-red-500";
+    const condition = percent > 50 ? "stable" : percent > 20 ? "strained" : "critical";
     return (
-        <div className={classNames("w-full", compact ? "text-[10px]" : "text-xs")}>
-            <div className="flex justify-between font-bold mb-1"><span>HP</span><span>{current}/{max}</span></div>
-            <div className="h-2 rounded-full bg-slate-200 overflow-hidden"><div className={classNames("h-full rounded-full transition-all", color)} style={{ width: `${percent}%` }} /></div>
+        <div className={`health-meter ${compact ? "health-meter-compact" : ""}`}>
+            <div className="health-meter-label"><span>Vitality</span><span>{current} / {max}</span></div>
+            <div className="health-meter-track"><div className={`health-meter-fill health-${condition}`} style={{ width: `${percent}%` }} /></div>
         </div>
     );
 };
