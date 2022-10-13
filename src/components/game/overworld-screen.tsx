431de98dfbec9e9279518bc4b4edbd6a5fa05fcc
yashimwong <yashimwong@gmail.com>
2022-10-13 10:15:00 +0800

    feat: restructure the survey workspace

--- src/components/game/overworld-screen.tsx
+++ src/components/game/overworld-screen.tsx
@@ -3,20 +3,24 @@ import OverworldMap from "components/game/overworld-map";
 import PokemonCard from "components/game/pokemon-card";
 import WorldLegend from "components/game/world-legend";
 import { GameState } from "game/types";
+import { FiArrowUpRight, FiCrosshair, FiMapPin } from "react-icons/fi";
 
 const OverworldScreen = ({ game, onMove, onOpenRoster }: { game: GameState; onMove: (dx: number, dy: number) => void; onOpenRoster: () => void }) => (
-    <div className="grid lg:grid-cols-[minmax(0,1fr)_270px] gap-6 items-start">
-        <section className="game-panel p-4 sm:p-6">
-            <div className="flex flex-wrap items-center gap-3 mb-5"><div><p className="text-xs font-bold uppercase tracking-wider text-indigo-600">Meadow Town</p><h1 className="text-2xl font-black">Explore, {game.trainerName}</h1></div><div className="ml-auto text-right"><p className="font-bold text-amber-600">◉ {game.money}</p><p className="text-xs text-slate-500">{game.badges.length} badge{game.badges.length === 1 ? "" : "s"}</p></div></div>
+    <div className="overworld-layout">
+        <section className="overworld-primary">
+            <header className="overworld-header">
+                <div><p className="eyebrow"><FiMapPin /> Sector 01 · Meadow basin</p><h1>Field survey</h1><p>Welcome back, Agent {game.trainerName}.</p></div>
+                <div className="overworld-telemetry"><div><span>POSITION</span><strong>{String(game.playerPosition.x).padStart(2, "0")} : {String(game.playerPosition.y).padStart(2, "0")}</strong></div><div><span>CREDITS</span><strong>₽ {game.money}</strong></div><div><span>INSIGNIA</span><strong>{String(game.badges.length).padStart(2, "0")}</strong></div></div>
+            </header>
+            <div className="map-toolbar"><span><FiCrosshair /> Live topographic feed</span><span>GRID / 16 × 12</span></div>
             <OverworldMap game={game} onMove={onMove} />
-            <p className="mt-4 text-center text-sm text-slate-500">Use arrow keys, WASD, or the controls to move. Walk in tall grass to find Pokémon.</p>
-            <WorldLegend />
-            <MovementControls onMove={onMove} />
+            <div className="map-footer"><WorldLegend /><MovementControls onMove={onMove} /></div>
         </section>
-        <aside className="game-panel p-5">
-            <div className="flex items-center justify-between"><h2 className="font-black text-lg">Your Party</h2><button type="button" onClick={onOpenRoster} className="text-sm font-bold text-indigo-600">View roster</button></div>
-            <div className="grid gap-3 mt-4">{game.party.map((pokemon, index) => <PokemonCard key={pokemon.uid} pokemon={pokemon} active={index === 0} />)}</div>
-            <div className="rounded-xl bg-indigo-50 p-4 mt-5 text-sm text-indigo-900"><p className="font-bold">Town guide</p><p className="mt-1">✚ heals your team. ● marks trainers looking for a battle.</p></div>
+        <aside className="overworld-sidebar">
+            <div className="sidebar-heading"><div><p className="eyebrow">Active unit</p><h2>Field party</h2></div><button type="button" onClick={onOpenRoster} aria-label="Open full specimen roster"><FiArrowUpRight /></button></div>
+            <div className="party-list">{game.party.map((pokemon, index) => <PokemonCard key={pokemon.uid} pokemon={pokemon} active={index === 0} />)}</div>
+            <div className="field-note"><span>FIELD NOTE / 08</span><p>Dense habitat begins beyond the marked trail. Encounters remain unpredictable.</p></div>
+            <div className="survey-progress"><div><span>REGION SURVEY</span><strong>{Math.min(92, 24 + game.defeatedTrainers.length * 18 + game.badges.length * 14)}%</strong></div><i><b style={{ width: `${Math.min(92, 24 + game.defeatedTrainers.length * 18 + game.badges.length * 14)}%` }} /></i></div>
         </aside>
     </div>
 );
