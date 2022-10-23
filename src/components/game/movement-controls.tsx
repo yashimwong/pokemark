b2de2e4a67c536c643bc0601a1d5387ce61ba7a8
yashimwong <yashimwong@gmail.com>
2022-10-23 10:15:00 +0800

    feat: refine survey controls and legend

--- src/components/game/movement-controls.tsx
+++ src/components/game/movement-controls.tsx
@@ -1,11 +1,13 @@
+import { FiArrowDown, FiArrowLeft, FiArrowRight, FiArrowUp } from "react-icons/fi";
+
 const MovementControls = ({ onMove }: { onMove: (dx: number, dy: number) => void }) => (
-    <div className="grid grid-cols-3 gap-1 w-36 mx-auto mt-5 select-none">
+    <div className="movement-controls">
         <span />
-        <button type="button" onClick={() => onMove(0, -1)} className="map-control">↑</button>
+        <button type="button" onClick={() => onMove(0, -1)} className="map-control" aria-label="Move north"><FiArrowUp /></button>
         <span />
-        <button type="button" onClick={() => onMove(-1, 0)} className="map-control">←</button>
-        <button type="button" onClick={() => onMove(0, 1)} className="map-control">↓</button>
-        <button type="button" onClick={() => onMove(1, 0)} className="map-control">→</button>
+        <button type="button" onClick={() => onMove(-1, 0)} className="map-control" aria-label="Move west"><FiArrowLeft /></button>
+        <button type="button" onClick={() => onMove(0, 1)} className="map-control" aria-label="Move south"><FiArrowDown /></button>
+        <button type="button" onClick={() => onMove(1, 0)} className="map-control" aria-label="Move east"><FiArrowRight /></button>
     </div>
 );
 
