58e757153f7e41eeeb47a41b43273c860ef97235
yashimwong <yashimwong@gmail.com>
2022-10-30 10:15:00 +0800

    feat: rebuild encounters as tactical scans

--- src/components/game/battle-pokemon.tsx
+++ src/components/game/battle-pokemon.tsx
@@ -1,4 +1,5 @@
 import HealthBar from "components/game/health-bar";
+import SpecimenPortrait from "components/game/specimen-portrait";
 import TypeBadge from "components/game/type-badge";
 import { species } from "game/data/species";
 import { Pokemon } from "game/types";
@@ -6,9 +7,13 @@ import { Pokemon } from "game/types";
 const BattlePokemon = ({ pokemon, opposing }: { pokemon: Pokemon; opposing?: boolean }) => {
     const currentSpecies = species[pokemon.speciesId];
     return (
-        <div className={`flex items-end gap-3 ${opposing ? "flex-row-reverse" : ""}`}>
-            <div className="battle-sprite" style={{ backgroundColor: `${currentSpecies.color}33` }}>{currentSpecies.sprite}</div>
-            <div className="battle-status"><div className="flex items-center gap-2"><strong>{pokemon.nickname}</strong><span className="text-xs text-slate-500">Lv. {pokemon.level}</span></div><div className="flex gap-1 mt-1">{currentSpecies.types.map((type) => <TypeBadge key={type} type={type} />)}</div><div className="mt-3"><HealthBar current={pokemon.hp} max={pokemon.maxHp} /></div></div>
+        <div className={`battle-pokemon ${opposing ? "battle-pokemon-opposing" : ""}`}>
+            <SpecimenPortrait specimen={currentSpecies} size="battle" opposing={opposing} />
+            <div className="battle-status">
+                <div className="battle-status-code">PM-{currentSpecies.number} / LEVEL {String(pokemon.level).padStart(2, "0")}</div>
+                <div className="battle-status-heading"><strong>{pokemon.nickname}</strong><div className="flex gap-1">{currentSpecies.types.map((type) => <TypeBadge key={type} type={type} />)}</div></div>
+                <HealthBar current={pokemon.hp} max={pokemon.maxHp} />
+            </div>
         </div>
     );
 };
--- src/components/game/battle-screen.tsx
+++ src/components/game/battle-screen.tsx
@@ -3,6 +3,7 @@ import BattleActions from "components/game/battle-actions";
 import BattlePokemon from "components/game/battle-pokemon";
 import PokemonCard from "components/game/pokemon-card";
 import { GameState, Move } from "game/types";
+import { FiActivity, FiArrowLeft } from "react-icons/fi";
 
 const BattleScreen = ({ game, onFight, onSwitch, onCatch, onRun }: { game: GameState; onFight: (move: Move) => void; onSwitch: (index: number) => void; onCatch: () => void; onRun: () => void }) => {
     const [switching, setSwitching] = useState(false);
@@ -13,13 +14,18 @@ const BattleScreen = ({ game, onFight, onSwitch, onCatch, onRun }: { game: GameS
     const opponentName = battle.opponent.kind === "wild" ? "Wild Pokémon" : `${battle.opponent.trainer?.title} ${battle.opponent.trainer?.name}`;
 
     return (
-        <div className="battle-scene game-panel overflow-hidden">
-            <div className="battle-header"><span>{opponentName}</span><span>{battle.opponent.kind === "wild" ? "A wild encounter" : "Trainer battle"}</span></div>
-            <div className="p-5 sm:p-8 min-h-[380px] flex flex-col justify-between gap-8">
+        <div className="battle-shell">
+            <div className="battle-header"><span><FiActivity /> Contact confirmed</span><strong>{opponentName}</strong><span>{battle.opponent.kind === "wild" ? "UNREGISTERED SPECIMEN" : "SANCTIONED DUEL"}</span></div>
+            <div className="battle-scene">
+                <span className="battle-axis battle-axis-horizontal" />
+                <span className="battle-axis battle-axis-vertical" />
                 <BattlePokemon pokemon={opponent} opposing />
                 <BattlePokemon pokemon={player} />
             </div>
-            <div className="battle-dialogue p-5 sm:p-6"><p className="font-bold text-slate-800 mb-4 min-h-[24px]">{battle.message}</p>{switching ? <div><p className="text-sm text-slate-500 mb-3">Choose a Pokémon to send out.</p><div className="grid sm:grid-cols-2 gap-3">{game.party.map((pokemon, index) => <PokemonCard key={pokemon.uid} pokemon={pokemon} active={index === battle.activePlayerIndex} onClick={() => { onSwitch(index); setSwitching(false); }} />)}</div><button type="button" onClick={() => setSwitching(false)} className="text-sm font-bold text-indigo-600 mt-4">Back to battle</button></div> : <BattleActions moves={player.moves} canCatch={battle.opponent.kind === "wild"} canRun={battle.canRun} onFight={onFight} onCatch={onCatch} onRun={onRun} onSwitch={() => setSwitching(true)} />}</div>
+            <div className="battle-console">
+                <div className="battle-message"><span>LIVE LOG / 01</span><p>{battle.message}</p></div>
+                {switching ? <div className="battle-switcher"><p>Select a healthy field partner.</p><div className="battle-switcher-grid">{game.party.map((pokemon, index) => <PokemonCard key={pokemon.uid} pokemon={pokemon} active={index === battle.activePlayerIndex} onClick={() => { onSwitch(index); setSwitching(false); }} />)}</div><button type="button" onClick={() => setSwitching(false)} className="battle-back"><FiArrowLeft /> Return to commands</button></div> : <BattleActions moves={player.moves} canCatch={battle.opponent.kind === "wild"} canRun={battle.canRun} onFight={onFight} onCatch={onCatch} onRun={onRun} onSwitch={() => setSwitching(true)} />}
+            </div>
         </div>
     );
 };
