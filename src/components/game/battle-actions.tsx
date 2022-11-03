f5960f81f3622a85795902ac3a0338bd497a1c5d
yashimwong <yashimwong@gmail.com>
2022-11-03 10:15:00 +0800

    feat: organize tactical battle commands

--- src/components/game/battle-actions.tsx
+++ src/components/game/battle-actions.tsx
@@ -1,12 +1,17 @@
 import TypeBadge from "components/game/type-badge";
 import { Move } from "game/types";
+import { FiArrowUpRight, FiCornerUpLeft, FiDisc, FiRepeat } from "react-icons/fi";
 
 const BattleActions = ({ moves, canCatch, canRun, onFight, onCatch, onRun, onSwitch }: { moves: Move[]; canCatch: boolean; canRun: boolean; onFight: (move: Move) => void; onCatch: () => void; onRun: () => void; onSwitch: () => void }) => (
-    <div className="grid sm:grid-cols-2 gap-3">
-        {moves.map((move) => <button type="button" key={move.id} onClick={() => onFight(move)} className="move-button"><span><strong>{move.name}</strong><small>{move.category} · {move.power} power</small></span><TypeBadge type={move.type} /></button>)}
-        <button type="button" onClick={onSwitch} className="battle-option">Switch Pokémon</button>
-        {canCatch && <button type="button" onClick={onCatch} className="battle-option">Throw Poké Ball</button>}
-        {canRun && <button type="button" onClick={onRun} className="battle-option">Run away</button>}
+    <div className="battle-actions">
+        <div className="move-grid">
+            {moves.map((move, index) => <button type="button" key={move.id} onClick={() => onFight(move)} className="move-button"><span className="move-index">0{index + 1}</span><span className="move-data"><strong>{move.name}</strong><small>{move.category} / {move.power} PWR</small></span><TypeBadge type={move.type} /><FiArrowUpRight className="move-arrow" /></button>)}
+        </div>
+        <div className="battle-options">
+            <button type="button" onClick={onSwitch} className="battle-option"><FiRepeat /><span>Rotate unit</span></button>
+            {canCatch && <button type="button" onClick={onCatch} className="battle-option"><FiDisc /><span>Deploy capture</span></button>}
+            {canRun && <button type="button" onClick={onRun} className="battle-option"><FiCornerUpLeft /><span>Withdraw</span></button>}
+        </div>
     </div>
 );
 
