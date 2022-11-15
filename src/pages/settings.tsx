b31222f65d5d906cab28d2bfd9c61dc7165fb920
yashimwong <yashimwong@gmail.com>
2022-11-15 10:15:00 +0800

    feat: recast journal as an expedition log

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,18 +1,18 @@
 import MainContainer from "components/layout/container";
 import { useGame } from "game/context";
+import { FiAward, FiBookOpen, FiMap, FiUser } from "react-icons/fi";
 
 const Settings = () => {
     const { game } = useGame();
     return (
         <MainContainer className="game-main">
-            <div className="w-full max-w-3xl mx-auto game-panel p-6 sm:p-8">
-                <p className="text-sm uppercase tracking-wider font-bold text-indigo-600">Trainer journal</p>
-                <h1 className="text-3xl font-black">Adventure settings</h1>
-                <div className="grid sm:grid-cols-2 gap-5 mt-7">
-                    <section className="rounded-2xl bg-slate-50 p-5"><h2 className="font-black">How to play</h2><ul className="text-sm text-slate-600 mt-3 space-y-2"><li>Move with arrow keys, WASD, or the map controls.</li><li>Walk in tall grass for random encounters.</li><li>Win trainer battles to earn Poké and badges.</li><li>Use healing stations to restore your party.</li></ul></section>
-                    <section className="rounded-2xl bg-indigo-50 p-5"><h2 className="font-black">Trainer record</h2><dl className="text-sm text-slate-600 mt-3 space-y-2"><div className="flex justify-between"><dt>Trainer</dt><dd className="font-bold">{game.trainerName || "New Trainer"}</dd></div><div className="flex justify-between"><dt>Poké</dt><dd className="font-bold">{game.money}</dd></div><div className="flex justify-between"><dt>Trainers defeated</dt><dd className="font-bold">{game.defeatedTrainers.length}</dd></div><div className="flex justify-between"><dt>Badges</dt><dd className="font-bold">{game.badges.length}</dd></div></dl></section>
+            <div className="log-shell">
+                <header className="log-header"><div><p className="eyebrow">Persistent field record</p><h1>Expedition log</h1><p>Operational notes, credentials, and earned distinctions.</p></div><span>JOURNAL / {game.trainerName ? game.trainerName.toUpperCase() : "UNASSIGNED"}</span></header>
+                <div className="log-grid">
+                    <section className="log-panel"><div className="log-panel-heading"><FiMap /><div><span>PROTOCOL 01</span><h2>Field procedure</h2></div></div><ol className="protocol-list"><li><span>01</span><p>Navigate using directional keys, WASD, or the field controls.</p></li><li><span>02</span><p>Survey dense habitat to identify unregistered specimens.</p></li><li><span>03</span><p>Complete sanctioned duels to earn funds and insignia.</p></li><li><span>04</span><p>Return to medical stations to stabilize your active unit.</p></li></ol></section>
+                    <section className="log-panel log-panel-dark"><div className="log-panel-heading"><FiUser /><div><span>IDENTITY</span><h2>Agent record</h2></div></div><dl className="agent-record"><div><dt>Call sign</dt><dd>{game.trainerName || "Not registered"}</dd></div><div><dt>Field credits</dt><dd>₽ {game.money}</dd></div><div><dt>Contacts cleared</dt><dd>{game.defeatedTrainers.length}</dd></div><div><dt>Registry volume</dt><dd>{game.party.length + game.storage.length}</dd></div></dl></section>
+                    <section className="log-panel log-panel-wide"><div className="log-panel-heading"><FiAward /><div><span>DISTINCTIONS</span><h2>Regional insignia</h2></div></div><div className="badge-registry">{game.badges.length ? game.badges.map((badge) => <div key={badge} className="badge-entry"><FiAward /><span><strong>{badge}</strong><small>MEADOW LEAGUE / VERIFIED</small></span></div>) : <div className="badge-empty"><FiBookOpen /><span><strong>No distinctions recorded</strong><small>Clear Ace Trainer Luna to qualify for regional review.</small></span></div>}</div></section>
                 </div>
-                <section className="mt-6"><h2 className="font-black">Badges</h2><div className="flex flex-wrap gap-3 mt-3">{game.badges.length ? game.badges.map((badge) => <span key={badge} className="rounded-full bg-amber-100 text-amber-800 px-4 py-2 text-sm font-bold">★ {badge}</span>) : <span className="text-sm text-slate-500">Defeat Ace Trainer Luna to earn the Meadow Badge.</span>}</div></section>
             </div>
         </MainContainer>
     );
