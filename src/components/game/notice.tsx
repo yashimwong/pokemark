16e0bb8796928a09cdcc428547fafa751b94b540
yashimwong <yashimwong@gmail.com>
2022-11-21 10:15:00 +0800

    style: turn notices into system alerts

--- src/components/game/notice.tsx
+++ src/components/game/notice.tsx
@@ -1,6 +1,8 @@
+import { FiX } from "react-icons/fi";
+
 const Notice = ({ message, onClose }: { message?: string; onClose: () => void }) => {
     if (!message) return null;
-    return <div className="fixed z-30 bottom-5 left-1/2 -translate-x-1/2 bg-slate-900 text-white rounded-xl shadow-xl px-5 py-4 max-w-sm w-[calc(100%-2rem)] flex items-center gap-4"><span className="flex-1 text-sm font-medium">{message}</span><button type="button" className="text-indigo-200 font-bold" onClick={onClose}>OK</button></div>;
+    return <div className="field-notice" role="status"><span className="field-notice-signal" /><div><small>FIELD SYSTEM</small><p>{message}</p></div><button type="button" onClick={onClose} aria-label="Dismiss notification"><FiX /></button></div>;
 };
 
 export default Notice;
